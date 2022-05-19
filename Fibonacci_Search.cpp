43e1bb2922c61cf72f4e471c07e8c73e9ae3e3d7
max2124 <max93.01.11@gmail.com>
2022-05-19 11:05:56 +0800

    finish Fibonacci_Search.cpp

--- Fibonacci_Search.cpp
+++ Fibonacci_Search.cpp
@@ -5,6 +5,7 @@ void Create_Fibonacci(int fibonacci[],int length){ // return y
 	fibonacci[0]=0;
 	fibonacci[1]=1;
 	for(int q=2;q<length;q++) fibonacci[q]=fibonacci[q-1] + fibonacci[q-2];
+	return;
 }
 int FindY(int fibonacci[],int length){
 	int q=0;
@@ -14,24 +15,31 @@ int FindY(int fibonacci[],int length){
 void Fibonacci_Search(int data[],int length,int search){
 	//Fy + m = n
 	int fibonacci[length];
-//	for(int q=0;q<length;q++) fibonacci[q]=INT_MIN;
+	for(int q=0;q<length;q++) fibonacci[q]=INT_MIN;
 	Create_Fibonacci(fibonacci,length);
-	for(int q=0;q<length;q++) cout<<fibonacci[q]<<' ';
-	cout<<'\n';
+//	for(int q=0;q<length;q++) cout<<fibonacci[q]<<' ';
+//	cout<<'\n';
 	int y=FindY(fibonacci,length+1);
 	int x=y-1;
 	int m=length-fibonacci[y];
 	int i=x;
 	if(data[x]<search) i+=m;
-	
+//	cout<<"x: "<<x<<" y: "<<y<<" m: "<<m<<" i: "<<i<<'\n';
+//	cout<<length<<' '<<fibonacci[y]<<'\n';
+//	system("pause");
+//	cout<<"m: "<<m<<'\n';
 	while(fibonacci[x]>0){
+//		cout<<"x: "<<x<<" fib: "<<fibonacci[x]<<" i: "<<i<<'\n';
 		if(data[i]<search) i+=fibonacci[--x];
 		else if(data[i]>search) i-=fibonacci[--x];
-		else{
+		else if(data[i] == search){
 			cout<<"資料 "<<search<<" 位在第 "<<i+1<<" 項\n";
 			return; 
 		}
+		if(i<0) i=0;
+		else if(i>=length) i=length-1;
 	}
+	cout<<"找不到 "<<search<<'\n';
 } 
 int main(){
 	int search,length;
